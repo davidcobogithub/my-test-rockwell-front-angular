a840eddfea538ca75b9d1516f3fef446bc221d1f
David Felipe Cobo Plazas <dcobo@intergrupo.com>
2021-08-26 22:08:25 -0500

    update files and add Procfile to deploy in prod environment

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/my-test-rockwell-dc'));
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/my-test-rockwell-dc/index.html'));
 });
 
