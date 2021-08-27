c3674895d5782a9a5a5016c1c21cf8cc2ed524e6
David Felipe Cobo Plazas <dcobo@intergrupo.com>
2021-08-26 21:50:50 -0500

    updates files to deploy in prod environment

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/my-test-rockwell-dc'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/my-test-rockwell-dc/index.html'));
+});
+
+app.listen(process.env.PORT || 4200);
